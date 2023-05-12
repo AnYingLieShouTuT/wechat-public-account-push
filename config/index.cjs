35a6a1d47788ef5a11cc7e13696823cfb7f489f7
AnYingLieShouTuT <108744191+AnYingLieShouTuT@users.noreply.github.com>
2023-05-12 20:59:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,7 +7,7 @@
  */
 const USER_CONFIG = {
   
-  USE_PASSAGE: 'push-plus',
+  USE_PASSAGE: 'server-chan',
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
@@ -23,28 +23,25 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '23f0e58eff96486e82e0f1991e9982ba',
+      id: 'SCT209761TOeynVeKE0EyAnS5dizMAEh4z',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '01-20',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '老婆生日', name: '老婆', year: '1999', date: '12-14',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '我的生日', name: '老公', year: '1999', date: '10-06',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-03-14' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
